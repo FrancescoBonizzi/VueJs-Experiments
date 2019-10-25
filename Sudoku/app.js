43f8b84a4790416e150ac68c118b66c854fe5502
Francesco Bonizzi <fbonizzi.90@gmail.com>
2019-10-25 10:05:04 +0200

    Sudoku

---
+++ Sudoku/app.js
@@ -0,0 +1,43 @@
+var defaultSudokuMatrix = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+];
+
+var sudokuApp = new Vue({
+	
+	el: '#app-sudoku',
+	
+	data: {
+
+        sudokuMatrix: [],
+        initializeGameText: "Start!"
+
+    },
+
+    methods: {
+
+        initializeGame() {
+
+            var sudokuMatrixToRandomize = defaultSudokuMatrix.slice();
+
+            // Empty random cells
+            for (var i = 0; i < sudokuMatrixToRandomize.length; ++i) {
+                for (var k = 0; k < 2; ++k) {
+                    var randomColumnIndex = Math.floor(Math.random() * sudokuMatrixToRandomize.length);
+                    sudokuMatrixToRandomize[i][randomColumnIndex] = "";
+                }
+            }
+
+            this.sudokuMatrix = sudokuMatrixToRandomize;
+            this.initializeGameText = "Restart";
+        }
+
+    }
+});
